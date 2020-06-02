802ea17f9d3daa0f013a64830a0c228e8b68934c
Dario Pellegrini <dario.pellegrini@s4win.com>
2020-06-02 20:44:11 +0200

    String extensions

---
+++ EasyString/EasyString.swift
@@ -0,0 +1,307 @@
+//
+//  EasyString.swift
+//  EasyString
+//
+//  Created by Dario Pellegrini on 02/06/2020.
+//  Copyright © 2020 Dario Pellegrini. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    // MARK: Index
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+    
+    func index(of character: Character) -> Int? {
+        return self.firstIndex(of: character)?.utf16Offset(in: self)
+    }
+    
+    func firstIndex(of string: String) -> Int? {
+        return self.range(of: string)?.lowerBound.utf16Offset(in: self)
+    }
+    
+    func lastIndex(of string: String) -> Int? {
+        return self.range(of: string, options: .backwards)?.lowerBound.utf16Offset(in: self)
+    }
+    
+    // MARK: Substrings
+    func substring(from: Int) -> String {
+        let fromIndex = index(from: from)
+        return self[fromIndex...].string
+    }
+
+    func substring(to: Int) -> String {
+        let toIndex = index(from: to)
+        return self[..<toIndex].string
+    }
+
+    func substring(range: Range<Int>) -> String {
+        let startIndex = index(from: range.lowerBound)
+        let endIndex = index(from: range.upperBound)
+        return self[startIndex..<endIndex].string
+    }
+    
+    func substring(before: String) -> String {
+        if let range = self.range(of: before) {
+            return self[self.startIndex..<range.lowerBound].string
+        }
+        return self
+    }
+    
+    func substring(after: String) -> String {
+        if let range = self.range(of: after) {
+            return self[range.upperBound...].string
+        }
+        return self
+    }
+    
+    func between(_ l: String, _ r: String) -> String {
+        if let rangeLeft = self.range(of: l),
+            let rangeRight = self.range(of: r) {
+            return self[rangeLeft.upperBound..<rangeRight.lowerBound].string
+        }
+        return self
+    }
+    
+    
+    // MARK: - Replace
+    func replace(_ occurencesOf: String, with: String) -> String {
+        return self.replacingOccurrences(of: occurencesOf, with: with)
+    }
+    
+    func replace(_ occurencesOf: String..., with: String) -> String {
+        var result = self
+        occurencesOf.forEach {
+            s in
+            result = result.replacingOccurrences(of: s, with: with)
+        }
+        return result
+    }
+    
+    func replace(regularExpression: String, with: String) -> String {
+        return self.replacingOccurrences(of: regularExpression, with: with, options: .regularExpression)
+    }
+    
+    // MARK: - Is number
+    var isNumber: Bool {
+        return !isEmpty && range(of: "[^0-9]", options: .regularExpression) == nil
+    }
+    
+    // MARK: - Is alphabetic
+    var isAlphabetic: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z]", options: .regularExpression) == nil
+    }
+    
+    // MARK: - Is alphanumeric
+    var isAlphanumeric: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    }
+    
+    // MARK: - Extractions of characters
+    var numberString: String {
+        return replace(regularExpression: "[^0-9]", with: "")
+    }
+    
+    var alphabeticString: String {
+        return replace(regularExpression: "[^a-zA-Z]", with: "")
+    }
+    
+    var alphanumericString: String {
+        return replace(regularExpression: "[^a-zA-Z0-9]", with: "")
+    }
+    
+    //MARK: - Charachter set
+    var set: CharacterSet {
+        return CharacterSet(charactersIn: self)
+    }
+    
+    // MARK: - String
+    func split(_ separator: String) -> [String] {
+        return self.components(separatedBy: separator)
+    }
+    
+    func split(_ set: CharacterSet) -> [String] {
+        return self.components(separatedBy: set)
+    }
+    
+    // MARK: - Camel case
+    var camelCase: String {
+        return self.lowercased()
+            .replace("-", with: " ")
+            .replace("_", with: " ")
+            .split(separator: " ")
+            .map { return $0.lowercased().capitalized }
+            .joined()
+    }
+    
+    // MARK: - Snake case
+    var snakeCase: String {
+        return self.lowercased()
+            .replace("-", with: " ")
+            .replace("_", with: " ")
+            .split(separator: " ")
+            .map { return $0.lowercased() }
+            .join("_")
+    }
+    
+    // MARK: - Latin
+    var latin: String {
+        return self.folding(options: .diacriticInsensitive, locale: .current)
+    }
+    
+    // MARK: - Lines
+    var lines: [String] {
+        return self.components(separatedBy: CharacterSet.newlines)
+    }
+    
+    // MARK: - Slugging
+    func slugify(withSeparator separator: String = "-") -> String {
+        let slugCharacterSet = NSCharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\(separator)")
+        return latin
+            .lowercased()
+            .components(separatedBy: slugCharacterSet.inverted)
+            .filter { $0 != "" }
+            .join(separator)
+       }
+    
+    // MARK: - Is empty
+    var isEmpty: Bool {
+        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).count == 0
+    }
+    
+    // MARK: - Collapse whitespaces
+    func collapseWhiteSpaces() -> String {
+        return components(separatedBy: CharacterSet.whitespacesAndNewlines)
+            .filter { $0.isEmpty == false }
+            .joined(separator: " ")
+    }
+    
+    // MARK: - Prefix
+    func has(prefix: String) -> Bool {
+        return self.hasPrefix(prefix)
+    }
+    
+    func remove(prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    func removePrefix(before: String, included: Bool = false) -> String {
+        guard let index = self.firstIndex(of: before) else { return self }
+        return self.substring(from: included == true ? index + 1 : index)
+    }
+    
+    // MARK: - Suffix
+    func has(suffix: String) -> Bool {
+        return self.hasSuffix(suffix)
+    }
+    
+    func remove(suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+    
+    func removeSuffix(after: String, included: Bool = false) -> String {
+        guard let index = self.lastIndex(of: after) else { return self }
+        return self.substring(to: included == true ? index : index + 1)
+    }
+    
+    // MARK: - Trim
+    func trim(in set: CharacterSet = CharacterSet.whitespacesAndNewlines) -> String {
+        return self.trimmingCharacters(in: set)
+    }
+    
+    func trimLeft(in set: CharacterSet = CharacterSet.whitespacesAndNewlines) -> String {
+        return self.replace(regularExpression: "^\\s+", with: "")
+    }
+    
+    func trimRight(in set: CharacterSet = CharacterSet.whitespacesAndNewlines) -> String {
+        return self.replace(regularExpression: "\\s+$", with: "")
+    }
+    
+    // MARK: - Pad
+    func pad(_ string: String, times: Int) -> String {
+        let pad = (0..<times).reduce("") {
+            acc, value in
+            "\(acc)\(string)"
+        }
+        return pad + self + pad
+    }
+    
+    func padLeft(_ string: String, times: Int) -> String {
+        return (0..<times).reduce("") {
+            acc, value in
+            "\(acc)\(string)"
+        } + self
+    }
+    
+    func padRight(_ string: String, times: Int) -> String {
+        return self + (0..<times).reduce("") {
+            acc, value in
+            "\(acc)\(string)"
+        }
+    }
+    
+    // MARK: - Number of occurencies
+    func count(string: String) -> Int {
+        return self.split(string).count - 1
+    }
+    
+    // MARK: - Conversions
+    func toBool() -> Bool? {
+        return Bool(self)
+    }
+    
+    func toInt() -> Int? {
+        return Int(self)
+    }
+    
+    func toFloat() -> Float? {
+        return Float(self)
+    }
+    
+    func toDouble() -> Double? {
+        return Double(self)
+    }
+    
+    // MARK: - Initials
+    var initials: String {
+        return self.components(separatedBy: CharacterSet.init(charactersIn: " -_\n\r")).map {
+            $0.substring(to: 1).capitalized
+        }.join(" ")
+    }
+    
+    // MARK: - Subscripts
+    subscript (bounds: CountableClosedRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start...end])
+    }
+
+    subscript (bounds: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start..<end])
+    }
+    
+    subscript(i: Int) -> String {
+        get {
+            return self.substring(range: i..<i+1)
+        }
+    }
+}
+
+extension Array where Element == String {
+    func join(_ separator: String) -> String {
+        return self.joined(separator: separator)
+    }
+}
+
+extension Substring {
+    public var string: String {
+        return String(self)
+    }
+}
--- EasyStringTests/EasyStringTests.swift
+++ EasyStringTests/EasyStringTests.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Dario Pellegrini. All rights reserved.
 //
 
+import EasyString
 import XCTest
 @testable import EasyString
 
@@ -17,6 +18,145 @@ class EasyStringTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testSubstring() throws {
+        var result = "Hello".substring(from: 1)
+        XCTAssert(result == "ello")
+        
+        result = "Hello".substring(to: 1)
+        XCTAssert(result == "H")
+        
+        result = "Hello".substring(range: 0..<2)
+        XCTAssert(result == "He")
+        
+        result = "Hello".substring(before: "e")
+        XCTAssert(result == "H")
+        
+        result = "Hello".substring(after: "l")
+        XCTAssert(result == "lo")
+        
+        result = "<Hello>".between("<", ">")
+        XCTAssert(result == "Hello")
+        
+        result = "<Hello>".replace("<", ">", with: "a")
+        XCTAssert(result == "aHelloa")
+        
+        XCTAssert("Hello".isNumber == false)
+        
+        XCTAssert("123".isNumber == true)
+        
+        XCTAssert("Hello5-".isAlphabetic == false)
+        
+        XCTAssert("Hello5".isAlphabetic == false)
+        
+        XCTAssert("Hello".isAlphabetic == true)
+        
+        XCTAssert("Hello123".isAlphanumeric == true)
+        
+        XCTAssert("123".isAlphanumeric == true)
+        
+        XCTAssert("123-".isAlphanumeric == false)
+        
+        result = "Hello1".numberString
+        XCTAssert(result == "1")
+        
+        result = "Hello1".alphabeticString
+        XCTAssert(result == "Hello")
+        
+        result = "Hello@1".alphanumericString
+        XCTAssert(result == "Hello1")
+        
+        result = "1,2,3,4,5".split(",").joined(separator: ";")
+        XCTAssert(result == "1;2;3;4;5")
+        
+        result = "1,2-3_4|5".split(",-_|".set).joined(separator: ";")
+        XCTAssert(result == "1;2;3;4;5")
+        
+        XCTAssert("123451".firstIndex(of: "1") == 0)
+        
+        XCTAssert("123451".lastIndex(of: "1") == 5)
+        
+        result = "Hello I-am_here".camelCase
+        XCTAssert(result == "HelloIAmHere")
+        
+        result = "Hello I-am_here".snakeCase
+        XCTAssert(result == "hello_i_am_here")
+        
+        result = "šÜįéïöç".latin
+        XCTAssert(result == "sUieioc")
+        
+        result = "Hello name šÜįéïöç".slugify()
+        XCTAssert(result == "hello-name-suieioc")
+        
+        result = " "
+        XCTAssert(result.isEmpty == true)
+        
+        result = "A "
+        XCTAssert(result.isEmpty == false)
+        
+        result = "Hello Planet   \n\n\n\r    Name".collapseWhiteSpaces()
+        XCTAssert(result == "Hello Planet Name")
+        
+        result = "XCHello"
+        XCTAssert(result.has(prefix: "XC") == true)
+        
+        result = "XCHello".remove(prefix: "XC")
+        XCTAssert(result == "Hello")
+        
+        result = "XC/Hello".removePrefix(before: "/", included: true)
+        XCTAssert(result == "Hello")
+        
+        result = "Hello.png"
+        XCTAssert(result.has(suffix: ".png") == true)
+        
+        result = "Hello.png".remove(suffix: ".png")
+        XCTAssert(result == "Hello")
+        
+        result = "Hello.png".removeSuffix(after: ".", included: true)
+        XCTAssert(result == "Hello")
+        
+        result = "  Hello   ".trim()
+        XCTAssert(result == "Hello")
+        
+        result = "  Hello   ".trimLeft()
+        XCTAssert(result == "Hello   ")
+        
+        result = "  Hello   ".trimRight()
+        XCTAssert(result == "  Hello")
+        
+        result = "Hello".pad("A", times: 3)
+        XCTAssert(result == "AAAHelloAAA")
+        
+        result = "Hello".padLeft("A", times: 3)
+        XCTAssert(result == "AAAHello")
+        
+        result = "Hello".padRight("A", times: 3)
+        XCTAssert(result == "HelloAAA")
+        
+        XCTAssert("Hello".count(string: "l") == 2)
+        
+        XCTAssert("true".toBool() == true)
+        
+        XCTAssert("1".toInt() == 1)
+        
+        XCTAssert("1.1".toFloat() == 1.1)
+        
+        XCTAssert("3.14".toDouble() == 3.14)
+        
+        result = "Hello Fir_st".initials
+        XCTAssert(result == "H F S")
+        
+        result = "Hello"[3]
+        XCTAssert(result == "l")
+        
+        result = "Hello"[2...3]
+        XCTAssert(result == "ll")
+    }
+    
+    func testRun() throws {
+        
+        // Subscript substring
     }
 
     func testExample() throws {

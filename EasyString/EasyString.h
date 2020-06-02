f7c1f6d4fb127ad4e6c65a7cfed2c9ec6e941081
Dario Pellegrini <dario.pellegrini@s4win.com>
2020-06-02 15:43:19 +0200

    Initial Commit

---
+++ EasyString/EasyString.h
@@ -0,0 +1,19 @@
+//
+//  EasyString.h
+//  EasyString
+//
+//  Created by Dario Pellegrini on 02/06/2020.
+//  Copyright © 2020 Dario Pellegrini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyString.
+FOUNDATION_EXPORT double EasyStringVersionNumber;
+
+//! Project version string for EasyString.
+FOUNDATION_EXPORT const unsigned char EasyStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyString/PublicHeader.h>
+
+

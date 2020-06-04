6332455c795c5d09c3ccc0f6985bdd8dc26bd168
Dario Pellegrini <dario.pellegrini@s4win.com>
2020-06-04 22:00:13 +0200

    Headers

---
+++ EasyString/EasyString.h
@@ -0,0 +1,12 @@
+//
+//  EasyString.h
+//  EasyString
+//
+//  Created by Dario Pellegrini on 04/06/2020.
+//  Copyright © 2020 Dario Pellegrini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double EasyStringVersionNumber;
+FOUNDATION_EXPORT const unsigned char EasyStringVersionString[];
